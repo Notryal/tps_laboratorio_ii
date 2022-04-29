5baae1d2b5be79a3d26f98b41759248d274bceff
Notryal <45153796+Notryal@users.noreply.github.com>
2022-04-29 15:00:17 -0300

    sigo arreglando cosas

--- Gonzalez.Ayrton.2A.TP2/Entidades/Vehiculo.cs
+++ Gonzalez.Ayrton.2A.TP2/Entidades/Vehiculo.cs
@@ -20,24 +20,25 @@ namespace Entidades
             Chico, Mediano, Grande
         }
         EMarca marca;
-
         string chasis;
         ConsoleColor color;
-
+           
+        /// <summary>
+        /// Constructor de parametros
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
         public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
         {
             this.marca = marca;
             this.chasis = chasis;
             this.color = color;
         }
-
         /// <summary>
         /// ReadOnly: Retornará el tamaño
         /// </summary>
-        protected abstract ETamanio Tamanio {
-            get;
-        }
-
+        protected abstract ETamanio Tamanio {get;}
         /// <summary>
         /// Publica todos los datos del Vehiculo.
         /// </summary>
@@ -46,7 +47,10 @@ namespace Entidades
         {
             return (String)this ;
         }
-
+        /// <summary>
+        /// Sobrecarga ToString
+        /// </summary>
+        /// <param name="p"></param>
         public static explicit operator string(Vehiculo p)
         {
             StringBuilder sb = new StringBuilder();
@@ -60,13 +64,12 @@ namespace Entidades
 
             return sb.ToString();
         }
-
         /// <summary>
         /// Dos vehiculos son iguales si comparten el mismo chasis
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <returns></returns>
+        /// <param name="v1">El primer vehiculo a comparar</param>
+        /// <param name="v2">El segundo vehiculo a comparar</param>
+        /// <returns>Retorna true si los atributos chasis de cada vehiculo son de igual valor, caso contrario retorna false</returns>
         public static bool operator ==(Vehiculo v1, Vehiculo v2)
         {
             return (v1.chasis == v2.chasis);
@@ -74,15 +77,13 @@ namespace Entidades
         /// <summary>
         /// Dos vehiculos son distintos si su chasis es distinto
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <returns></returns>
+        /// <param name="v1">El primer vehiculo a comparar</param>
+        /// <param name="v2">El segundo vehiculo a comparar</param>
+        /// <returns>Retorna true si los atributos chasis de cada vehiculo son de diferente valor, caso contrario retorna false</returns>
         public static bool operator !=(Vehiculo v1, Vehiculo v2)
         {
-            return (v1.chasis == v2.chasis);
+            return !(v1 == v2);
         }
-
-
 
     }
 }
